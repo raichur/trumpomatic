6b8a56bd05325550f7eb21f69adfcf5acfbca103
Joshua Raichur <raichur@utexas.edu>
2017-09-14 00:45:16 -0500

    api with gettweet func

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from app import markov
 from flask import render_template
+from flask import jsonify
 import os.path
 
 @app.route('/')
@@ -12,4 +13,13 @@ def index():
     tweets = open(tweets_url, "r")
     trumpkov = markov.Markov(tweets)
     tweet_text = trumpkov.generate_markov_text()
-    return render_template('index.html', markovtext=tweet_text)
+    return render_template('index.html', SCRIPT_ROOT=script_path)
+
+@app.route('/gettext')
+def gettext():
+    script_path = os.path.dirname(__file__)
+    tweets_url = "".join([script_path, '/static/tweets.txt'])
+    tweets = open(tweets_url, "r")
+    trumpkov = markov.Markov(tweets)
+    tweet_text = trumpkov.generate_markov_text()
+    return jsonify(tweet_text)
